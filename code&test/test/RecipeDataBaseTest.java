a09c01e3ae8c0f2219e21f992943ed5e03741412
Dan Nguyen <ndan0112@gmail.com>
2023-04-19 17:05:32 -0500

    Test case created for Add,Exist, and Search for Recipe Database and Initzilizing Recipe Init

--- code&test/test/RecipeDataBaseTest.java
+++ code&test/test/RecipeDataBaseTest.java
@@ -1,5 +1,48 @@
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class RecipeDataBaseTest extends TestCase {
+    //test add recipe
+    @Test
+    public void testAddRecipe() {
+        RecipeDataBase recipeDataBase = new RecipeDataBase();
+        Recipe recipe = new Recipe("test", new String[]{"test"}, "test", new String[]{"test"});
+        recipeDataBase.addRecipe(recipe);
+        assertEquals(recipeDataBase.recipes[0], recipe);
+    }
+    //test search recipe by name
+    @Test
+    public void testSearchRecipeByName() {
+        RecipeDataBase recipeDataBase = new RecipeDataBase();
+        Recipe recipe = new Recipe("test", new String[]{"test"}, "test", new String[]{"test"});
+        recipeDataBase.addRecipe(recipe);
+        assertEquals(recipeDataBase.searchRecipeByName("test"), recipe);
+    }
+
+    //test recipe exist
+    @Test
+    public void testRecipeExist() {
+        RecipeDataBase recipeDataBase = new RecipeDataBase();
+        Recipe recipe = new Recipe("test", new String[]{"test"}, "test", new String[]{"test"});
+        recipeDataBase.addRecipe(recipe);
+        assertTrue(recipeDataBase.recipeExist("test"));
+        assertFalse(recipeDataBase.recipeExist("test2"));
+    }
+
+    //Test recipe initiation
+    @Test
+    public void testRecipeInit() {
+        Recipe recipe = new Recipe("test", new String[]{"test"}, "test", new String[]{"test"});
+        assertEquals(recipe.name, "test");
+        assertEquals(recipe.ingredients[0], "test");
+        assertEquals(recipe.category, "test");
+        assertEquals(recipe.instructions[0], "test");
+        assertNotSame(recipe.name, "test2");
+        assertNotSame(recipe.ingredients[0], "test2");
+        assertNotSame(recipe.category, "test2");
+        assertNotSame(recipe.instructions[0], "test2");
+        assertNull(recipe.image);
+    }
+
 
 }
